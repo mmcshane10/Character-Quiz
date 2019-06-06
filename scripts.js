69f17f4f14054bc1c1534b063a64c246bfeacfd8
mmcshane10 <49597817+mmcshane10@users.noreply.github.com>
2019-06-05 20:30:31 -0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -5,13 +5,14 @@ $(document).ready(function() {
     event.preventDefault();
 
     var operator = $("input:radio[name=operator]:checked").val();
+    var adventure = $("input:radio[name=adventure]:checked").val();
 
-    if (operator === "Yes") {
+    if (operator === "Yes" && adventure === "Yes") {
       character = "Dr. Alan Grant";
     } else if (operator === "No") {
       character = "Newman"
     }
-
+console.log()
     $("#character").empty().append(character);
       $("#result").show();
 
